dff709ae07b04a0090bcbf09f9d8c1f5cb8db9d1
Uche Ngadi <uchengadi@yahoo.com>
2022-07-18 16:58:16 +0100

    redirect baseurl to production

--- src/apis/local.js
+++ src/apis/local.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:8000/api/v1",
-  baseURL: "http://35.180.217.158/api/v1",
+  baseURL: "http://3.11.60.228/api/v1",
 });
